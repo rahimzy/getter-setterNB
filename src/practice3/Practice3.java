f44fb423817daa06aa7fe98ac8a5b6f05375d1e6
Breshna-Rahimzy-Sheridan <rahimzy@sheridancollege.ca>
2019-03-04 22:28:21 -0500

    initializing number 10.

---
+++ src/practice3/Practice3.java
@@ -0,0 +1,23 @@
+/*
+ * Breshna Rahimzy
+ * Student ID: 991487145
+ *     SYST10199 - WEB Programming
+ */
+package practice3;
+
+/**
+ *
+ * @author USER
+ */
+public class Practice3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int num = 10;
+        System.out.println(num);
+    }
+    
+}
